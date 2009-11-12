3aef6c36e6b92566748ae768e0ff019dd8280bd9
Ryan Tomayko <rtomayko@gmail.com>
2009-11-12 06:52:13 -0800

    add liquid tests - beer is wet but whisky is wetter

--- test/spec_tilt_liquid_template.rb
+++ test/spec_tilt_liquid_template.rb
@@ -18,6 +18,45 @@ begin
       template = Tilt::LiquidTemplate.new { "Hey {{ name }}!" }
       template.render(nil, :name => 'Joe').should.equal "Hey Joe!"
     end
+
+    # Object's passed as "scope" to LiquidTemplate may respond to
+    # #to_h with a Hash. The Hash's contents are merged underneath
+    # Tilt locals.
+    class ExampleLiquidScope
+      def to_h
+        { :beer => 'wet', :whisky => 'wetter' }
+      end
+    end
+
+    it "merges scope Hash in under locals when scope responds to #to_h" do
+      template =
+        Tilt::LiquidTemplate.new {
+          'Beer is {{ beer }} but Whisky is {{ whisky }}.'
+        }
+      scope = ExampleLiquidScope.new
+      template.render(scope).should.equal "Beer is wet but Whisky is wetter."
+    end
+
+    it "gives locals presendence over scope defined variables" do
+      template =
+        Tilt::LiquidTemplate.new {
+          'Beer is {{ beer }} but Whisky is {{ whisky }}.'
+        }
+      scope = ExampleLiquidScope.new
+      template.render(scope, :beer => 'great', :whisky => 'greater').
+        should.equal "Beer is great but Whisky is greater."
+    end
+
+    # Object's passed as "scope" to LiquidTemplate that do not
+    # respond to #to_h are silently ignored.
+    class ExampleIgnoredLiquidScope
+    end
+
+    it "does not freak out when scope does not respond to #to_h" do
+      template = Tilt::LiquidTemplate.new { 'Whisky' }
+      scope = ExampleIgnoredLiquidScope.new
+      template.render(scope).should.equal "Whisky"
+    end
   end
 
 rescue LoadError => boom
