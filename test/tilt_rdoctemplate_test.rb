4897192ef7a5c48fd210babe349d650cd306e1cc
Magnus Holm <judofyr@gmail.com>
2011-01-16 19:38:07 +0100

    Make RDoc tests whitespace insensitive (closes #56)

--- test/tilt_rdoctemplate_test.rb
+++ test/tilt_rdoctemplate_test.rb
@@ -11,12 +11,12 @@ begin
 
     test "preparing and evaluating the template with #render" do
       template = Tilt::RDocTemplate.new { |t| "= Hello World!" }
-      assert_equal "<h1>Hello World!</h1>\n", template.render
+      assert_equal "<h1>Hello World!</h1>", template.render.strip
     end
 
     test "can be rendered more than once" do
       template = Tilt::RDocTemplate.new { |t| "= Hello World!" }
-      3.times { assert_equal "<h1>Hello World!</h1>\n", template.render }
+      3.times { assert_equal "<h1>Hello World!</h1>", template.render.strip }
     end
   end
 rescue LoadError => boom
