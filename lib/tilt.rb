47f6eeb355d9cf3645c8d5bab248e835e9c6268e
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-04-15 13:25:43 +0200

    add redcarpet support

--- lib/tilt.rb
+++ lib/tilt.rb
@@ -174,6 +174,7 @@ module Tilt
   register MarukuTemplate,    'markdown', 'mkd', 'md'
   register KramdownTemplate,  'markdown', 'mkd', 'md'
   register BlueClothTemplate, 'markdown', 'mkd', 'md'
+  register RedcarpetTemplate, 'markdown', 'mkd', 'md'
   register RDiscountTemplate, 'markdown', 'mkd', 'md'
 
   require 'tilt/textile'
