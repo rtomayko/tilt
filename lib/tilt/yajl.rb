4dcc20b89836ee45c0c2dc83d152195eacd712f5
Simone Carletti <weppos@weppos.net>
2011-05-03 18:13:27 +0200

    Refactor the code to use a buffer variable.
    In this way, the template is even more flexible.
    I added some documentation to document the features.

--- lib/tilt/yajl.rb
+++ lib/tilt/yajl.rb
@@ -9,6 +9,35 @@ module Tilt
   #
   # The template source is evaluated as a Ruby string,
   # and the result is converted #to_json.
+  #
+  # == Example
+  #
+  #    # This is a template example.
+  #    # The template can contain any Ruby statement.
+  #    tpl <<-EOS
+  #      @counter = 0
+  #
+  #      # The json variable represents the buffer
+  #      # and holds the data to be serialized into json.
+  #      # It defaults to an empty hash, but you can override it at any time.
+  #      json = {
+  #        :"user#{@counter += 1}" => { :name => "Joshua Peek", :id => @counter },
+  #        :"user#{@counter += 1}" => { :name => "Ryan Tomayko", :id => @counter },
+  #        :"user#{@counter += 1}" => { :name => "Simone Carletti", :id => @counter },
+  #      }
+  #
+  #      # Since the json variable is a Hash,
+  #      # you can use conditional statements or any other Ruby statement
+  #      # to populate it.
+  #      json[:"user#{@counter += 1}"] = { :name => "Unknown" } if 1 == 2
+  #
+  #      # The last line doesn't affect the returned value.
+  #      nil
+  #    EOS
+  #
+  #    template = Tilt::YajlTemplate.new { tpl }
+  #    template.render(self)
+  #
   class YajlTemplate < Template
 
     self.default_mime_type = 'application/json'
@@ -22,16 +51,26 @@ module Tilt
     end
 
     def prepare
-      @encoder = Yajl::Encoder.new
     end
 
     def evaluate(scope, locals, &block)
-      decorate @encoder.encode(super(scope, locals, &block)), options
+      obj = super(scope, locals, &block)
+      decorate Yajl::Encoder.new.encode(obj), options
+    end
+
+    def precompiled_preamble(locals)
+      return super if locals.include? :json
+      "json = {}\n#{super}"
+    end
+
+    def precompiled_postamble(locals)
+      "json"
     end
 
     def precompiled_template(locals)
       data.to_str
     end
+
 
     # Decorates the +json+ input according to given +options+.
     #
