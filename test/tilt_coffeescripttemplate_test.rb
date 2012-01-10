37834111ee65971bdaf889baa8d30b35aadf1987
Joshua Peek <josh@joshpeek.com>
2012-01-10 10:47:11 -0600

    Revert "Merge pull request #124 from hannesg/coffee_script_test_fix"
    This reverts commit b9dc4e56384ccd35e188c444913bd79f57a05331, reversing
    changes made to 77dfce203c113c6a4cfc51a9703e76cb53f871b2.

--- test/tilt_coffeescripttemplate_test.rb
+++ test/tilt_coffeescripttemplate_test.rb
@@ -23,16 +23,15 @@ begin
       str = "puts 'Hello, World!'\n"
 
       template = Tilt::CoffeeScriptTemplate.new(:bare => true) { str }
-      assert_equal "\nputs('Hello, World!');\n", template.render
+      assert_equal "puts('Hello, World!');", template.render
 
       template2 = Tilt::CoffeeScriptTemplate.new(:no_wrap => true) { str}
-      assert_equal "\nputs('Hello, World!');\n", template.render
+      assert_equal "puts('Hello, World!');", template.render
     end
 
     context "disabling coffee-script wrapper globally" do
       setup do
         @bare = Tilt::CoffeeScriptTemplate.default_bare
-        Tilt::CoffeeScriptTemplate.default_bare = true
       end
 
       teardown do
@@ -41,17 +40,18 @@ begin
 
       test "no options" do
         template = Tilt::CoffeeScriptTemplate.new { |t| "puts 'Hello, World!'\n" }
-        assert_equal "\nputs('Hello, World!');\n", template.render
+        assert_match "puts('Hello, World!');", template.render
+        assert_match "(function() {", template.render
       end
 
       test "overridden by :bare" do
         template = Tilt::CoffeeScriptTemplate.new(:bare => false) { "puts 'Hello, World!'\n" }
-        assert_not_equal "\nputs('Hello, World!');\n", template.render
+        assert_not_equal "puts('Hello, World!');", template.render
       end
 
       test "overridden by :no_wrap" do
         template = Tilt::CoffeeScriptTemplate.new(:no_wrap => false) { "puts 'Hello, World!'\n" }
-        assert_not_equal "\nputs('Hello, World!');\n", template.render
+        assert_not_equal "puts('Hello, World!');", template.render
       end
     end
   end
