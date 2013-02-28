a05543f0b98ec5390894a0e435ef1f10b052fdc9
Sebastian Skałacki <skalee@gmail.com>
2013-02-28 05:47:04 +0100

    Cleanup CoffeeScript tests
    Make them DRY before introducing tests for literate CoffeeScript
    handler, extract template renderer class to instance variable.

--- test/tilt_coffeescripttemplate_test.rb
+++ test/tilt_coffeescripttemplate_test.rb
@@ -6,64 +6,68 @@ begin
 
   class CoffeeScriptTemplateTest < Minitest::Test
 
+    setup do
+      @code_without_variables = "puts 'Hello, World!'\n"
+      @code_with_variables = 'name = "Josh"; puts "Hello, #{name}"'
+      @renderer = Tilt::CoffeeScriptTemplate
+    end
+
     test "is registered for '.coffee' files" do
-      assert_equal Tilt::CoffeeScriptTemplate, Tilt['test.coffee']
+      assert_equal @renderer, Tilt['test.coffee']
     end
 
     test "bare is disabled by default" do
-      assert_equal false, Tilt::CoffeeScriptTemplate.default_bare
+      assert_equal false, @renderer.default_bare
     end
 
     test "compiles and evaluates the template on #render" do
-      template = Tilt::CoffeeScriptTemplate.new { |t| "puts 'Hello, World!'\n" }
+      template = @renderer.new { |t| @code_without_variables }
       assert_match "puts('Hello, World!');", template.render
     end
 
     test "can be rendered more than once" do
-      template = Tilt::CoffeeScriptTemplate.new { |t| "puts 'Hello, World!'\n" }
+      template = @renderer.new { |t| @code_without_variables }
       3.times { assert_match "puts('Hello, World!');", template.render }
     end
 
     test "disabling coffee-script wrapper" do
-      str = 'name = "Josh"; puts "Hello #{name}"'
-
-      template = Tilt::CoffeeScriptTemplate.new { str }
+      template = @renderer.new { @code_with_variables }
       assert_match "(function() {", template.render
-      assert_match "puts(\"Hello \" + name);\n", template.render
+      assert_match "puts(\"Hello, \" + name);\n", template.render
 
-      template = Tilt::CoffeeScriptTemplate.new(:bare => true) { str }
-      refute_match "(function() {", template.render
-      assert_equal "var name;\n\nname = \"Josh\";\n\nputs(\"Hello \" + name);\n", template.render
+      template = @renderer.new(:bare => true) { @code_with_variables }
+      assert_not_match "(function() {", template.render
+      assert_equal "var name;\n\nname = \"Josh\";\n\nputs(\"Hello, \" + name);\n", template.render
 
-      template2 = Tilt::CoffeeScriptTemplate.new(:no_wrap => true) { str}
-      refute_match "(function() {", template.render
-      assert_equal "var name;\n\nname = \"Josh\";\n\nputs(\"Hello \" + name);\n", template.render
+      template2 = @renderer.new(:no_wrap => true) { @code_with_variables}
+      assert_not_match "(function() {", template.render
+      assert_equal "var name;\n\nname = \"Josh\";\n\nputs(\"Hello, \" + name);\n", template.render
     end
 
     context "wrapper globally enabled" do
       setup do
-        @bare = Tilt::CoffeeScriptTemplate.default_bare
-        Tilt::CoffeeScriptTemplate.default_bare = false
+        @bare = @renderer.default_bare
+        @renderer.default_bare = false
       end
 
       teardown do
-        Tilt::CoffeeScriptTemplate.default_bare = @bare
+        @renderer.default_bare = @bare
       end
 
       test "no options" do
-        template = Tilt::CoffeeScriptTemplate.new { |t| 'name = "Josh"; puts "Hello, #{name}"' }
+        template = @renderer.new { |t| @code_with_variables }
         assert_match "puts(\"Hello, \" + name);", template.render
         assert_match "(function() {", template.render
       end
 
       test "overridden by :bare" do
-        template = Tilt::CoffeeScriptTemplate.new(:bare => true) { |t| 'name = "Josh"; puts "Hello, #{name}"' }
+        template = @renderer.new(:bare => true) { |t| @code_with_variables }
         assert_match "puts(\"Hello, \" + name);", template.render
         refute_match "(function() {", template.render
       end
 
       test "overridden by :no_wrap" do
-        template = Tilt::CoffeeScriptTemplate.new(:no_wrap => true) { |t| 'name = "Josh"; puts "Hello, #{name}"' }
+        template = @renderer.new(:no_wrap => true) { |t| @code_with_variables }
         assert_match "puts(\"Hello, \" + name);", template.render
         refute_match "(function() {", template.render
       end
@@ -71,28 +75,28 @@ begin
 
     context "wrapper globally disabled" do
       setup do
-        @bare = Tilt::CoffeeScriptTemplate.default_bare
-        Tilt::CoffeeScriptTemplate.default_bare = true
+        @bare = @renderer.default_bare
+        @renderer.default_bare = true
       end
 
       teardown do
-        Tilt::CoffeeScriptTemplate.default_bare = @bare
+        @renderer.default_bare = @bare
       end
 
       test "no options" do
-        template = Tilt::CoffeeScriptTemplate.new { |t| 'name = "Josh"; puts "Hello, #{name}"' }
+        template = @renderer.new { |t| @code_with_variables }
         assert_match "puts(\"Hello, \" + name);", template.render
         refute_match "(function() {", template.render
       end
 
       test "overridden by :bare" do
-        template = Tilt::CoffeeScriptTemplate.new(:bare => false) { |t| 'name = "Josh"; puts "Hello, #{name}"' }
+        template = @renderer.new(:bare => false) { |t| @code_with_variables }
         assert_match "puts(\"Hello, \" + name);", template.render
         assert_match "(function() {", template.render
       end
 
       test "overridden by :no_wrap" do
-        template = Tilt::CoffeeScriptTemplate.new(:no_wrap => false) { |t| 'name = "Josh"; puts "Hello, #{name}"' }
+        template = @renderer.new(:no_wrap => false) { |t| @code_with_variables }
         assert_match "puts(\"Hello, \" + name);", template.render
         assert_match "(function() {", template.render
       end
