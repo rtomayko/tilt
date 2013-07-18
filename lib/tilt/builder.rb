83a2c683eb874ebcd147d9afc5d7cedcfd4a8486
Marcello Barnaba <vjt@openssl.it>
2013-07-18 16:22:56 +0200

    Add ability to override the Builder indent amount

--- lib/tilt/builder.rb
+++ lib/tilt/builder.rb
@@ -11,14 +11,14 @@ module Tilt
 
     def evaluate(scope, locals, &block)
       return super(scope, locals, &block) if data.respond_to?(:to_str)
-      xml = ::Builder::XmlMarkup.new(:indent => 2)
+      xml = ::Builder::XmlMarkup.new(:indent => indent_amount)
       data.call(xml)
       xml.target!
     end
 
     def precompiled_preamble(locals)
       return super if locals.include? :xml
-      "xml = ::Builder::XmlMarkup.new(:indent => 2)\n#{super}"
+      "xml = ::Builder::XmlMarkup.new(:indent => #{indent_amount})\n#{super}"
     end
 
     def precompiled_postamble(locals)
@@ -28,6 +28,13 @@ module Tilt
     def precompiled_template(locals)
       data.to_str
     end
+
+    private
+
+    def indent_amount
+      options[:indent] || 2
+    end
+
   end
 end
 
