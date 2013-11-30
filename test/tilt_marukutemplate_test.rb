5377a23e277e9622452a713d617d2674af43e9e1
Magnus Holm <judofyr@gmail.com>
2013-11-30 20:42:41 +0100

    Fix Maruku template tests

--- test/tilt_marukutemplate_test.rb
+++ test/tilt_marukutemplate_test.rb
@@ -17,18 +17,18 @@ begin
 
     test "preparing and evaluating templates on #render" do
       template = Tilt::MarukuTemplate.new { |t| "# Hello World!" }
-      assert_equal "<h1 id='hello_world'>Hello World!</h1>", template.render
+      assert_equal "<h1 id=\"hello_world\">Hello World!</h1>", template.render.strip
     end
 
     test "can be rendered more than once" do
       template = Tilt::MarukuTemplate.new { |t| "# Hello World!" }
-      3.times { assert_equal "<h1 id='hello_world'>Hello World!</h1>", template.render }
+      3.times { assert_equal "<h1 id=\"hello_world\">Hello World!</h1>", template.render.strip }
     end
 
     test "removes HTML when :filter_html is set" do
       template = Tilt::MarukuTemplate.new(:filter_html => true) { |t|
         "HELLO <blink>WORLD</blink>" }
-      assert_equal "<p>HELLO </p>", template.render
+      assert_equal "<p>HELLO </p>", template.render.strip
     end
   end
 rescue LoadError => boom
