ce440a7574892e0b21dc52965b69af0423cfad66
Joshua Muheim <josh@josh.ch>
2016-05-11 16:19:52 +0200

    Add mapping for tilt options :smartypants and :escape_html

--- lib/tilt/pandoc.rb
+++ lib/tilt/pandoc.rb
@@ -6,7 +6,29 @@ module Tilt
   # http://pandoc.org/
   class PandocTemplate < Template
     def prepare
-      @output = PandocRuby.convert(data, {:to => :html}, *options).strip
+      @output = PandocRuby.convert(data, options_hash.merge(
+                                           {:to => :html}
+                                         ), *options_array
+                                  ).strip
+    end
+
+    def tilt_to_pandoc_mapping
+      { :smartypants => :smart,
+        :escape_html => :markdown_strict
+      }
+    end
+
+    def options_array
+      options.map do |option|
+        tilt_to_pandoc_mapping[option[0]] || option[0] if option[1] === true
+      end.compact
+    end
+
+    def options_hash
+      options.inject({}) do |hash, option|
+        hash[option[0]] = option[1] unless option[1] === true
+        hash
+      end
     end
 
     def evaluate(scope, locals, &block)
--- test/tilt_pandoctemplate_test.rb
+++ test/tilt_pandoctemplate_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'tilt'
 
 begin
-  require 'tilt/maruku'
+  require 'tilt/pandoc'
 
   class PandocTemplateTest < Minitest::Test
     test "registered below Kramdown" do
@@ -25,13 +25,40 @@ begin
       3.times { assert_equal "<h1 id=\"hello-world\">Hello World!</h1>", template.render.strip }
     end
 
+    test "smartypants when :smartypants is set" do
+      template = Tilt::PandocTemplate.new(:smartypants => true) { |t| "OKAY -- 'Smarty Pants'" }
+      assert_equal "<p>OKAY – ‘Smarty Pants’</p>", template.render
+    end
+
+    test "stripping HTML when :escape_html is set" do
+      skip "Couldn't find appropriate option for Pandoc, see http://stackoverflow.com/questions/37165374/pandoc-escape-html-option"
+      template = Tilt::PandocTemplate.new(:escape_html => true) { |t| "HELLO <blink>WORLD</blink>" }
+      assert_equal "<p>HELLO &lt;blink>WORLD&lt;/blink></p>", template.render
+    end
+
+    # Pandoc has tons of additional markdown features.
+    # The test for footnotes should be see as a general representation for all of them.
     test "generates footnotes" do
       template = Tilt::PandocTemplate.new { |t| "Here is an inline note.^[Inlines notes are cool!]" }
       assert_equal "<p>Here is an inline note.<a href=\"#fn1\" class=\"footnoteRef\" id=\"fnref1\"><sup>1</sup></a></p>\n<div class=\"footnotes\">\n<hr />\n<ol>\n<li id=\"fn1\"><p>Inlines notes are cool!<a href=\"#fnref1\">↩</a></p></li>\n</ol>\n</div>", template.render.strip
     end
 
-    test "passes in Pandoc options" do
-      # TODO
+    describe "passing in Pandoc options" do
+      test "accepts arguments with values" do
+        # Table of contents isn't on by default
+        template = Tilt::PandocTemplate.new { |t| "# This is a heading" }
+        assert_equal "<h1 id=\"this-is-a-heading\">This is a heading</h1>", template.render
+
+        # But it can be activated
+        template = Tilt::PandocTemplate.new(id_prefix: 'test-') { |t| "# This is a heading" }
+        assert_equal "<h1 id=\"test-this-is-a-heading\">This is a heading</h1>", template.render
+      end
+
+      # Arguments without value (e.g. --standalone) need to be passed as hash keys, too (simply set them to true)
+      test "requires arguments without value as true values" do
+        template = Tilt::PandocTemplate.new(standalone: true) { |t| "# This is a heading" }
+        assert_match /^<!DOCTYPE html.*<h1 id="this-is-a-heading">This is a heading<\/h1>.*<\/html>$/m, template.render
+      end
     end
   end
 rescue LoadError => boom
