15d3faf18f5728cdcaf102f5ad7aa03306988e5c
Jacob Francis Powers <xxv9xx@gmail.com>
2016-06-25 08:05:51 -0500

    remove hard config

--- lib/tilt/livescript.rb
+++ lib/tilt/livescript.rb
@@ -9,20 +9,7 @@ module Tilt
   class LiveScriptTemplate < Template
     self.default_mime_type = 'application/javascript'
 
-    @@default_bare = false
-
-    def self.default_bare
-      @@default_bare
-    end
-
-    def self.default_bare=(value)
-      @@default_bare = value
-    end
-
     def prepare
-      if !options.key?(:bare)
-        options[:bare] = self.class.default_bare
-      end
     end
 
     def evaluate(scope, locals, &block)
