68eab4d0710ba77f36a134f40f25332a9b914495
Akira Matsuda <ronnie@dio.jp>
2016-12-14 20:44:45 +0900

    Suppress "assigned but unused variable - locals" warning
    Without this dirty hack, ruby emits "assigned but unused variable - locals" warning
    in VERBOSE mode when no locals are given.

--- lib/tilt/template.rb
+++ lib/tilt/template.rb
@@ -261,6 +261,7 @@ module Tilt
             Thread.current[:tilt_vars] = [self, locals]
             class << self
               this, locals = Thread.current[:tilt_vars]
+              locals = locals
               this.instance_eval do
                 #{local_code}
       RUBY
