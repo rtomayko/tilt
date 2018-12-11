adac3ec8c97d862254f363a7ce3e47a6c8fe88d0
Ivan Nemytchenko <nemytchenko@gmail.com>
2018-12-11 20:23:02 +0100

    proper params list

--- lib/tilt/hamdown.rb
+++ lib/tilt/hamdown.rb
@@ -16,7 +16,7 @@ module Tilt
     end
 
     def evaluate(scope, locals, &block)
-      @output ||= Hamdown::Engine.perform(data, options, &block)
+      @output ||= Hamdown::Engine.perform(data)
     end
 
     def allows_script?
