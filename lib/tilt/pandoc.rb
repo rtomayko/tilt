48375b8ee065dd6991bc747e7cccc9439fe2d696
Magnus Holm <judofyr@gmail.com>
2022-07-18 21:29:25 +0200

    Pandoc: Handle the new way of specifying smartypants

--- lib/tilt/pandoc.rb
+++ lib/tilt/pandoc.rb
@@ -7,30 +7,38 @@ module Tilt
   class PandocTemplate < Template
     self.default_mime_type = 'text/html'
 
-    def tilt_to_pandoc_mapping
-      { :smartypants => :smart,
-        :escape_html => { :f => 'markdown-raw_html' },
-        :commonmark => { :f => 'commonmark' },
-        :markdown_strict => { :f => 'markdown_strict' }
-      }
-    end
-
     # turn options hash into an array
     # Map tilt options to pandoc options
     # Replace hash keys with value true with symbol for key
     # Remove hash keys with value false
     # Leave other hash keys untouched
     def pandoc_options
-      options.reduce([]) do |sum, (k,v)|
-        case v
-        when true
-          sum << (tilt_to_pandoc_mapping[k] || k)
-        when false
-          sum
+      result = []
+      from = "markdown"
+      smart_extension = "-smart"
+      options.each do |k,v|
+        case k
+        when :smartypants
+          smart_extension = "+smart" if v
+        when :escape_html
+          from = "markdown-raw_html" if v
+        when :commonmark
+          from = "commonmark" if v
+        when :markdown_strict
+          from = "markdown_strict" if v
         else
-          sum << { k => v }
+          case v
+          when true
+            result << k
+          when false
+            # do nothing
+          else
+            result << { k => v }
+          end
         end
       end
+      result << { :f => from + smart_extension }
+      result
     end
 
     def prepare
