bf1a460e92284ed4850e37760cf6d72f1eb2a3b6
Magnus Holm <judofyr@gmail.com>
2022-07-18 21:11:29 +0200

    AsciiDoctor: Remove test for deprecated docbook45

--- test/tilt_asciidoctor_test.rb
+++ test/tilt_asciidoctor_test.rb
@@ -6,7 +6,6 @@ begin
 
   class AsciidoctorTemplateTest < Minitest::Test
     HTML5_OUTPUT = "<div class=\"sect1\"><h2 id=\"_hello_world\">Hello World!</h2><div class=\"sectionbody\"></div></div>"
-    DOCBOOK45_OUTPUT = "<section id=\"_hello_world\"><title>Hello World!</title></section>"
     DOCBOOK5_OUTPUT = "<section xml:id=\"_hello_world\"><title>Hello World!</title></section>"
 
     def strip_space(str)
@@ -28,11 +27,6 @@ begin
     test "preparing and evaluating html5 templates on #render" do
       template = Tilt::AsciidoctorTemplate.new(:attributes => {"backend" => 'html5'}) { |t| "== Hello World!" } 
       assert_equal HTML5_OUTPUT, strip_space(template.render)
-    end
-
-    test "preparing and evaluating docbook 4.5 templates on #render" do
-      template = Tilt::AsciidoctorTemplate.new(:attributes => {"backend" => 'docbook45'}) { |t| "== Hello World!" }
-      assert_equal DOCBOOK45_OUTPUT, strip_space(template.render)
     end
 
     test "preparing and evaluating docbook 5 templates on #render" do
