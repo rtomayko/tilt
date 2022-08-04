0de713df6f5230f5154718881a8536344e3e635d
Jeremy Evans <code@jeremyevans.net>
2022-08-04 11:01:45 -0700

    Chomp newline in sass tests
    In my environment these fail due to render returning a string ending
    with a newline.  Since the newline doesn't cause any problems in
    CSS, I don't think we should force remove it in the template engine.
    It seems acceptable to allow output both with and without a newline.

--- test/tilt_sasstemplate_test.rb
+++ test/tilt_sasstemplate_test.rb
@@ -10,12 +10,12 @@ begin
 
     test "compiles and evaluates the template on #render" do
       template = Tilt::SassTemplate.new({ style: :compressed }) { |t| "#main\n  background-color: #0000f1" }
-      assert_equal "#main{background-color:#0000f1}", template.render
+      assert_equal "#main{background-color:#0000f1}", template.render.chomp
     end
 
     test "can be rendered more than once" do
       template = Tilt::SassTemplate.new({ style: :compressed }) { |t| "#main\n  background-color: #0000f1" }
-      3.times { assert_equal "#main{background-color:#0000f1}", template.render }
+      3.times { assert_equal "#main{background-color:#0000f1}", template.render.chomp }
     end
   end
 
@@ -26,12 +26,12 @@ begin
 
     test "compiles and evaluates the template on #render" do
       template = Tilt::ScssTemplate.new({ style: :compressed }) { |t| "#main {\n  background-color: #0000f1;\n}" }
-      assert_equal "#main{background-color:#0000f1}", template.render
+      assert_equal "#main{background-color:#0000f1}", template.render.chomp
     end
 
     test "can be rendered more than once" do
       template = Tilt::ScssTemplate.new({ style: :compressed }) { |t| "#main {\n  background-color: #0000f1;\n}" }
-      3.times { assert_equal "#main{background-color:#0000f1}", template.render }
+      3.times { assert_equal "#main{background-color:#0000f1}", template.render.chomp }
     end
   end
 
