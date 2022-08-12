rtomayko/tilt

commit a168cc9e7ae86c6e9769c4692ee7153b885906ef

Pandoc: Handle the new way of specifying smartypants

lib/tilt/rst-pandoc.rb
@@ -1,18 +1,23 @@
 require 'tilt/template'
-require 'tilt/pandoc'
+require 'pandoc'
 
 module Tilt
   # Pandoc reStructuredText implementation. See:
   # http://pandoc.org/
-  # Use PandocTemplate and specify input format
   class RstPandocTemplate < PandocTemplate
-    def tilt_to_pandoc_mapping
-      { :smartypants => :smart }
+    self.default_mime_type = 'text/html'
+
+    def prepare
+      @engine = PandocRuby.new(data, :f => "rst")
+      @output = nil
     end
 
-    def pandoc_options
-      options.merge!(f: 'rst')
-      super
+    def evaluate(scope, locals, &block)
+      @output ||= @engine.to_html.strip
+    end
+
+    def allows_script?
+      false
     end
   end
 end

lib/tilt/pandoc.rb
@@ -7,30 +7,38 @@ module Tilt
   class PandocTemplate < Template
     self.default_mime_type = 'text/html'
 
-    def tilt_to_pandoc_mapping
-      { :smartypants => :smart,
-        :escape_html => { :f => 'markdown-raw_html' },
-        :commonmark => { :f => 'commonmark' },
-        :markdown_strict => { :f => 'markdown_strict' }
-      }
-    end
-
     # turn options hash into an array
     # Map tilt options to pandoc options
     # Replace hash keys with value true with symbol for key
     # Remove hash keys with value false
     # Leave other hash keys untouched
     def pandoc_options
-      options.reduce([]) do |sum, (k,v)|
-        case v
-        when true
-          sum << (tilt_to_pandoc_mapping[k] || k)
-        when false
-          sum
+      result = []
+      from = "markdown"
+      smart_extension = "-smart"
+      options.each do |k,v|
+        case k
+        when :smartypants
+          smart_extension = "+smart" if v
+        when :escape_html
+          from = "markdown-raw_html" if v
+        when :commonmark
+          from = "commonmark" if v
+        when :markdown_strict
+          from = "markdown_strict" if v
         else
-          sum << { k => v }
+          case v
+          when true
+            result << k
+          when false
+            # do nothing
+          else
+            result << { k => v }
+          end
         end
       end
+      result << { :f => from + smart_extension }
+      result
     end
 
     def prepare

commit a6684d9ccb9d2c8067e7906ad250e782c778fbe1

Fix broken rst-pandoc template
This template wasn't tested in CI, and has been broken since
a168cc9e7ae86c6e9769c4692ee7153b885906ef changed the require.

With the require fixed, their was one test failure, where it
appears the :escape_html works now, so I updated the test.

lib/tilt/rst-pandoc.rb
@@ -1,5 +1,5 @@
 require 'tilt/template'
-require 'pandoc'
+require_relative 'pandoc'
 
 module Tilt
   # Pandoc reStructuredText implementation. See:

test/tilt_rstpandoctemplate_test.rb
@@ -20,10 +20,9 @@ begin
       end
     end
 
-    it "doens't use markdown options" do
+    it "supports :escape_html option" do
       template = Tilt::RstPandocTemplate.new(:escape_html => true) { |t| "HELLO <blink>WORLD</blink>" }
-      err = assert_raises(RuntimeError) { template.render }
-      assert_match %r(pandoc: unrecognized option `--escape-html), err.message
+      assert_equal "<p>HELLO &lt;blink&gt;WORLD&lt;/blink&gt;</p>", template.render
     end
   end
 rescue LoadError => boom